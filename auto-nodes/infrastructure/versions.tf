fbaf88ca5e89e5427aa6e7a10729cbecd26aeddc
Jannis Schoormann <jannis.schoormann@external.biontech.de>
2026-01-12 16:29:00 +0100

    add infrastructure templates for auto node provisioning

---
+++ auto-nodes/infrastructure/versions.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_version = ">= 1.5"
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 6.50.0"
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "~> 6.50.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.7.2"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.2.4"
+    }
+  }
+}
